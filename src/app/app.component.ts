06c01716e325d5372add29ae169cf5462b379dfa
weboey <583287168@qq.com>
2018-03-22 19:24:58 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,53 @@ export class AppComponent {
   }
 
   doSearch(val){
-    console.log("out pull==>"+val);
+    console.log("Search==>"+val);
+    console.log(this.initVal);
+    if(val!=""){
+      let reg=new RegExp(`${val}`,"i");
+      this.testDate=this.mockDate.filter((item)=>{
+        if(item["bond_shortname"].match(reg)){
+          return item
+        }
+      })
+    }else {
+      this.testDate=[];
+    }
   }
-
+  test(){
+    this.initVal={name:"00资金509"};
+  }
   doOpen(isOpen){
-    console.log("out pull==>"+isOpen);
+    console.log("Open==>"+isOpen);
   }
+  change(val){
+    console.log("change"+val);
+  }
+  isDisable=false;
+  initVal={name:"00资金09"};
+  testDate=[];
+  mockDate=[{
+    "bond_shortname": "00国债01",
+  }, {
+    "bond_shortname": "00国债02",
+
+  }, {
+    "bond_shortname": "00国债03",
+  }, {
+    "bond_shortname": "00国债09",
+  }, {
+    "bond_shortname": "00资金01",
+  }, {
+    "bond_shortname": "00资金09",
+
+  }, {
+    "bond_shortname": "00资金019",
+  }, {
+    "bond_shortname": "00资金309",
+  }, {
+    "bond_shortname": "00资金509",
+  }, {
+    "bond_shortname": "00资金809",
+  }];
+
 }
--- src/app/search-select/search-select.component.ts
+++ src/app/search-select/search-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter ,ViewChild,ElementRef,AfterContentInit,ContentChildren,
+import { Component, OnInit, Input, Output, EventEmitter ,OnDestroy,ViewChild,ElementRef,AfterContentInit,ContentChildren,
   Directive,QueryList} from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs';
@@ -7,128 +7,192 @@ import "rxjs/add/operator/debounceTime";
 import "rxjs/add/operator/distinctUntilChanged";
 import "rxjs/add/operator/switch";
 import "rxjs/add/operator/filter";
+import { coerceBooleanProperty} from '@angular/cdk/coercion';
 
 @Directive({ selector: 'li' })
 export class ListItem {}
 
 @Component({
   selector: 'app-search-select',
-  templateUrl: './search-select.component2.html',
-  styleUrls: ['./search-select.component2.scss'],
+  templateUrl: './search-select.component.html',
+  styleUrls: ['./search-select.component.scss'],
+  host: {
+    '[class.ant-select]'            : 'true',
+    "(click)": "_toggleClick($event)",
+  }
 })
-export class SearchSelectComponent implements OnInit,AfterContentInit {
+export class SearchSelectComponent implements OnInit,AfterContentInit,OnDestroy {
 
-
-  inputValue:string="";
   selectedIndex:number=0;
-  toggleShow:boolean=false;
-  mockDate=[1,2,3,4,5,6,7,8];
+  _optionListShow:boolean=false;
 
   @Output()
   nzSearchChange: EventEmitter<any> = new EventEmitter<any>();
   @Output()
   nzOpenChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output()
+  ngModelChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   @Input()
   nzNotFoundContent:any;
   @Input()
   nzPlaceHolder:string="";
-  private searchText$ = new Subject<string>();
-
-  @ViewChild("inputElement") inpElementRef: ElementRef;
-
-  @ContentChildren(ListItem) items: QueryList<ListItem>;
-
-  onKeyDownInput(e){
-    console.log(e);
-  }
-
-  ngAfterContentInit():void {
-    // do something with list items
-    console.log(this.items);
-
-  }
-
-  toggleShowHandler(){
-    this.toggleShow=!this.toggleShow;
-    this.nzOpenChange.emit(this.toggleShow);
-  }
-
-  doSelectItem(selected){
-    this.updateInputVal(selected);
-    this.selectedIndex=this.mockDate.indexOf(selected);
-  }
-
-  doSearch(inputVal){
-    this.searchText$.next(inputVal);
-  }
-
-  doClose(){
-    setTimeout(()=>{
-      this.toggleShow=false;
-    },200)
-  }
-
-
-  updateInputVal(newVal){
-      this.inputValue=newVal;
-  }
-
-  public tipInfo = [];
-  // 例子
-  // public data = [{key: 'bond', placeholder: '示范课搜索', search: '模拟搜索'}];
-  // 搜索数据
-
-  // 回填文字
   @Input()
-  public infoBond;
-  // 搜索的字段和内容
-  @Output()
-  public keyData: EventEmitter<any> = new EventEmitter;
-  // 搜索的字段和内容
-  @Output()
-  public searchData: EventEmitter<any> = new EventEmitter;
-  @Output()
-  public searchName: EventEmitter<any> = new EventEmitter;
-  // 获取的数据和字段
-  public data: Array<any>;
-  // 输入框提示文字
-  public tip_name: string;
-  // 搜索按钮文字
-  public search_name: string;
-  // 输入框内容
-  public organization;
-  // 箭头下选的位置
-  public SelectInstIndex = null;
-  // 是否显示下拉框
-  public showTip = false;
-  // 字段
-  public keyCode: string;
+  nzOptionLabel:string;
+  @Input()
+  nzOptionValue:string;
+
+  private _value: any; // select表单值
+  @Output() valueChange = new EventEmitter();
+  @Input()
+  public get value(): any {
+    return this._value;
+  }
+  public set value(newValue: any) {
+    //this.writeValue(newValue);
+    this._value=newValue;
+    this.valueChange.emit(this.value);
+  }
+
+  private _data: Array<object>=[];
+  @Input()
+  public get data(): Array<object> | object[] {
+    return this._data;
+  }
+  public set data(value: Array<object> | object[]) {
+    this._data = value instanceof Array ? value : new Array(value);
+    let selectedItem=this.data.find(item=>{
+      console.log(item[this.nzOptionLabel],this.value);
+      return item[this.nzOptionLabel]==this.value
+    })
+    this.selectedIndex=this.data.indexOf(selectedItem);
+  }
+
 
   constructor() { }
 
+  _allowClear = false;
+  _disabled = false;
+  _clearIcon = false;
+  @Input()
+  set nzAllowClear(value: boolean) {
+    this._allowClear = coerceBooleanProperty(value);
+  }
+  get nzAllowClear(): boolean {
+    return this._allowClear;
+  }
+  @Input()
+  set nzDisabled(value: boolean) {
+    this._disabled = coerceBooleanProperty(value);
+  }
+  get nzDisabled(): boolean {
+    return this._disabled;
+  }
+
+  _showClear(event: Event){
+    event.stopPropagation();
+    if(this._value==null || this._value=='' || this._disabled){
+      return
+    }
+    this._clearIcon=true;
+  }
+  _hiddenClear(event: Event){
+    event.stopPropagation();
+    this._clearIcon=false;
+  }
+
+  private sub$: any;
+  @ViewChild("inputElement") inpElementRef: ElementRef;
+  @ContentChildren(ListItem) items: QueryList<ListItem>;
+
+  ngAfterContentInit():void {
+    console.log(this.items);
+  }
+
+  _toggleClick(event: Event){
+    event.stopPropagation();
+    if(this._disabled){
+      return
+    }
+    this._clearIcon=false;
+    this._optionListShow =! this._optionListShow;
+    this.nzOpenChange.emit(this._optionListShow);
+    if(this._optionListShow){
+      this.nzPlaceHolder=this.value;
+    }else{
+
+    }
+    // if (this._$optionListHidden) {
+    //   this._documentListen();
+    // } else {
+    //   this._documentListen = this._renderer.listen('document', 'click', () => {
+    //     this._$optionListHidden = true;
+    //     this._documentListen();
+    //     this._documentListen = null;
+    //   });
+    // }
+  }
+
+  onClearSelected(event){
+    event.stopPropagation();
+    this._value=null;
+  }
+
+
+  //更改option选中状态
+  _updateSelectedOption(selected){
+    this._updateInputVal(selected);
+    this.selectedIndex=this.data.indexOf(selected);
+  }
+  //关闭下拉面板
+  _closeOptionContent(){
+    setTimeout(()=>{
+      this._optionListShow=false;
+    },200)
+  }
+
+  //更新input值
+  _updateInputVal(newVal){
+    this.value=newVal[this.nzOptionLabel];
+    this.ngModelChange.emit(this.value);
+  }
 
   ngOnInit() {
-    // const input$ = Observable.fromEvent<KeyboardEvent>(this.inpElement["nativeElement"], 'keydown');
-    //this.searchText$
-    //  .debounceTime(300)
-    //  .filter(r => r !== '')
-    //  .distinctUntilChanged()
-    //  .switch()
-    //  .subscribe(inp=>console.log(inp))
-    let inpuyKeyUp$ = Observable.fromEvent(this.inpElementRef.nativeElement, 'keyup')
-      .map((e: any) => e.target.value) // extract the value of the input
-      .filter((text: string) => text!='') // filter out if empty
+    this.sub$ = Observable.fromEvent<KeyboardEvent>(this.inpElementRef.nativeElement, 'keyup')
+      .map((e: any) => e.target.value)
+      // .filter((text: string) => text!='') // filter out if empty
       .do((val)=>console.log(val))
-      .debounceTime(250) // only once every 250ms
+      .debounceTime(300)
       .distinctUntilChanged()
-      // act on the return of the search
       .subscribe(
-        inpVal => { // on sucesss
+        inpVal => {
           console.log(inpVal);
           this.nzSearchChange.next(inpVal);
         }
       );
   }
 
+  //键盘上下选择
+  onKeyDownChangeSelected(event) {
+    if (this._optionListShow && event.keyCode === 38) {
+      this.selectedIndex = this.selectedIndex ? this.selectedIndex - 1 : this.data.length - 1;
+      if (this.selectedIndex < 0) {
+        this.selectedIndex = this.data.length - 1;
+      }
+    } else if (this._optionListShow && event.keyCode === 40) {
+      this.selectedIndex = this.selectedIndex || this.selectedIndex === 0 ? this.selectedIndex + 1 : 0;
+      if (this.selectedIndex > this.data.length - 1) {
+        this.selectedIndex = 0;
+      }
 
+    } else if (this._optionListShow && event.keyCode === 13) {
+      event.preventDefault();
+      this._updateSelectedOption(this.data[this.selectedIndex]);
+      this._optionListShow=false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
+  }
 }
